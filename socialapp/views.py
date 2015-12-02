d161ab7a81156612e47baae0af26612e055b4a41
TudorRotarus25 <tudor.rotarus@gmail.com>
2015-12-02 22:53:50 +0200

    Tema 4 finala - comments

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from socialapp.models import UserPost
-from socialapp.forms import UserPostForm
+from socialapp.models import UserPost, UserPostComment
+from socialapp.forms import UserPostForm, UserPostCommentForm
 
 
 def index(request):
@@ -24,5 +24,21 @@ def index(request):
 
 def post_details(request, pk):
     post = UserPost.objects.get(pk=pk)
-    context = {'post': post}
+
+    if request.method == 'GET':
+        form = UserPostCommentForm()
+        comm = UserPostComment.objects.filter(post=post).order_by('-date_added')
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comm,
+        }
+    elif request.method == 'POST':
+        form = UserPostCommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            comment = UserPostComment(text=text, post=post)
+            comment.save()
+        return redirect('post_details', post.pk)
+
     return render(request, 'post_details.html', context)
